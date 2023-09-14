7277dd039c751bbc1c2e3663985dc4570acbc047
yqzhishen <yangqian_1015@icloud.com>
2023-09-14 23:24:12 +0800

    Support bidirectional EMD loss

--- modules/losses/emd.py
+++ modules/losses/emd.py
@@ -2,10 +2,15 @@ import torch.nn
 
 
 class BinaryEMDLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, bidirectional=False):
         super().__init__()
         self.loss = torch.nn.L1Loss()
+        self.bidirectional = bidirectional
 
     def forward(self, pred, gt):
         # pred, gt: [B, T]
-        return self.loss(pred.cumsum(dim=1), gt.cumsum(dim=1))
+        loss = self.loss(pred.cumsum(dim=1), gt.cumsum(dim=1))
+        if self.bidirectional:
+            loss += self.loss(pred.flip(dim=1).cumsum(dim=1), gt.flip(dim=1).cumsum(dim=1))
+            loss /= 2
+        return loss
