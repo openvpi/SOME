8f1527eebbc2321f7f174e2ed24b7e455cd6df11
autumn <109412646+autumn-2-net@users.noreply.github.com>
2023-09-17 13:23:35 +0800

    add quant config

--- training/__init__.py
+++ training/__init__.py
@@ -1,3 +1,4 @@
 from .base_task import BaseTask
 from .me_task import MIDIExtractionTask
-from .me_task_b import MIDIExtractionTask as MET
+from .me_quant_task import QuantizedMIDIExtractionTask
+# from .me_task_b import MIDIExtractionTask as MET
